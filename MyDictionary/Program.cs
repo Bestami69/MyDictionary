f7cc36d7d550960d6be886fafc192bb90097f6a0
Bestami Tayfuroğlu <bestami.tayfuroglu@hotmail.com>
2021-01-30 15:14:19 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dictonary = new MyDictionary<int, string>();
+
+            dictonary.Add(1, "Bayburt");
+            dictonary.Add(2, "İstanbul");
+            dictonary.Add(3, "İzmir");
+            dictonary.Add(4, "Erzurum");
+
+            Console.WriteLine(dictonary.Count);
+            Console.ReadKey();
+        }
+    }
+}
